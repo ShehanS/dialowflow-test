50809a5a7ae4e6fae7ad8adac2e77bfe5819bdd2
shehan <shehan.salinda92@gmail.com>
2023-06-15 12:50:20 +0530

    initials

---
+++ src/main/java/com/shehan/dialgoflow/demo/InfoController.java
@@ -0,0 +1,21 @@
+package com.shehan.dialgoflow.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class InfoController {
+    @GetMapping(path = "getUsers")
+    public List<String> getUser() {
+        List<String> users = new ArrayList<>();
+        users.add("Shehan");
+        users.add("Amila");
+        users.add("Kasun");
+        users.add("Chamath");
+        return users;
+
+    }
+}
---
+++ src/test/java/com/shehan/dialgoflow/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shehan.dialgoflow.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
