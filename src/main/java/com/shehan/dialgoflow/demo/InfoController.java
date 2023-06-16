6763180d2bfacad7f44649e86907cbd8194b9cad
shehan <shehan.salinda92@gmail.com>
2023-06-16 10:03:49 +0530

    change list type

--- src/main/java/com/shehan/dialgoflow/demo/InfoController.java
+++ src/main/java/com/shehan/dialgoflow/demo/InfoController.java
@@ -5,21 +5,25 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class InfoController {
     @GetMapping(path = "getUsers")
-    public List<String> getUser() {
+    public  Map<String, List<String>> getUser() {
+        Map<String, List<String>> usersList = new HashMap<>();
         List<String> users = new ArrayList<>();
         users.add("Shehan");
         users.add("Amila");
         users.add("Kasun");
         users.add("Chamath");
-        return users;
+        usersList.put("user", users);
+        return usersList;
     }
 
-    @PostMapping
+    @PostMapping("api")
     public Object requestApi(Object request){
         System.out.println("------------------LOGS-----------------");
         System.out.println(request);
